03dda296ed31e566a5c0220226cb5823f6d57a20
rom4nz <kavishkaigalagama@gmail.com>
2025-09-02 18:47:34 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/my-portfolio-website/',
   plugins: [
     react(),
     tailwindcss(),
   ],
-  base: '/my-portfolio-website/',
 })
